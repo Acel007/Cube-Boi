5385c4c75b8442c40b2499fcbd755f9326113216
Acel <xxacel007xx@gmail.com>
2020-01-22 20:21:04 +0100

    Basic Camera Movement
    The Camera can now move along the x-z Axis

---
+++ Assets/Scripts/Behaviour/CamMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+	
+    private Camera playerCam;
+
+    public float moveSpeed = 1.0f;
+    public float rotationSpeed = 1.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        playerCam = GetComponent<Camera>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        moveCamera();
+
+    }
+
+    private void moveCamera()
+    {
+        float xMovement = Input.GetAxis("Horizontal")    * moveSpeed * Time.deltaTime;
+        float zMovement = Input.GetAxis("Vertical")      * moveSpeed * Time.deltaTime;
+
+        playerCam.transform.Translate(xMovement, zMovement, 0);
+    }
+    
+}
